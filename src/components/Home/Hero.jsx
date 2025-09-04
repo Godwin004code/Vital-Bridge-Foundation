badaa3ec1a7dd41b5a4378a3c24051e09895f691
Godwin004code <godwinatomicsoda@gmail.com>
2025-09-04 12:51:23 +0100

    all

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -10,10 +10,10 @@ const Hero = () => {
         <img src={hero} alt="hero" className='object-cover h-full w-full' />
         <section className='w-full h-full bg-[#00000099] absolute top-0'>
            <section className='w-[90%] mx-auto mt-20'>
-           <h2 className='text-white text-6xl sm:text-3xl font-bold'>INVESTING IN PEOPLE;  
+           <h2 className='text-white text-6xl md-2:text-3xl font-bold'>INVESTING IN PEOPLE;  
 </h2>
-            <h2 className='text-white text-6xl  sm:text-3xl font-bold'>TRANSFORMING LIVES;</h2>
-            <h2 className='text-white text-6xl  sm:text-3xl font-bold'>INSPIRING PURPOSEFUL PROGRESS</h2>
+            <h2 className='text-white text-6xl  md-2:text-3xl font-bold'>TRANSFORMING LIVES;</h2>
+            <h2 className='text-white text-6xl  md-2:text-3xl font-bold'>INSPIRING GODLY & PURPOSEFUL PROGRESS</h2>
             <Link to="/what-we-do">
             <button className='w-[148px] h-[51px] bg-[#720000] mt-10 text-white rounded-sm'>
            What we do

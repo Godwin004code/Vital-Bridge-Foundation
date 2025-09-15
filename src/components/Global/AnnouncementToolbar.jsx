95b4add188d12780408cee2883d3aaab796b0f93
Godwin004code <godwinatomicsoda@gmail.com>
2025-09-15 17:17:00 +0100

    hdhdh

--- src/components/Global/AnnouncementToolbar.jsx
+++ src/components/Global/AnnouncementToolbar.jsx
@@ -5,7 +5,8 @@ const AnnouncementToolbar = () => {
     return (
         <div className="announcement-toolbar">
             <div className="announcement-text">
-                Your dream; we fund. Together, your dream can become. Apply for our ongoing university scholarship today!
+
+                You dream; we fund. With us, your dream can become. Apply for our ongoing university scholarship today!
             </div>
         </div>
     );
